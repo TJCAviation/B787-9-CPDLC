b7b63d46d09038df8b839226515930e7e4555567
TJCAviation <TJC.Aviation@gmail.com>
2023-07-28 16:30:01 +0100

    Fixed ALT Display

--- HS_B787-9/html_ui/Pages/VCockpit/Instruments/Airliners/HSB787_9/ALT/HSB787_9_ALT.js
+++ HS_B787-9/html_ui/Pages/VCockpit/Instruments/Airliners/HSB787_9/ALT/HSB787_9_ALT.js
@@ -1,5 +1,5 @@
-class B787_9\_ALT extends Boeing_FCU.ALT {
-    get templateID() { return "HSB789_ALT"; }
+class B787_9_ALT extends Boeing_FCU.ALT {
+    get templateID() { return "B787_9_ALT"; }
     get instrumentAlias() { return "AS01B_ALT"; }
 }
 registerInstrument("b787-9-alt-element", B787_9_ALT);
