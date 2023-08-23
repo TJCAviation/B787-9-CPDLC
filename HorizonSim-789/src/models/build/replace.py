48376266ce97e0e8b52581ead1eebaabc911bcc0
Yago Martínez <65448292+TheRealQbit@users.noreply.github.com>
2023-08-23 13:10:00 +0200

    fixed more gltf building code

--- HorizonSim-789/src/models/build/replace.py
+++ HorizonSim-789/src/models/build/replace.py
@@ -16,7 +16,7 @@ data_lines = data_lines.replace('"material": 66,', '"material": 21,')#FUSELAGE2/
 data_lines = data_lines.replace('"material": 65,', '"material": 23,')#LIVERY1_3/DONE
 data_lines = data_lines.replace('"material": 67,', '"material": 24,')#HANDLE/DONE
 data_lines = data_lines.replace('"material": 63,', '"material": 22,')#GLASS
-data_lines = data_lines.replace('"material": 62,', '"material": 29,')#HUBLOTx
+data_lines = data_lines.replace('"material": 61,', '"material": 29,')#HUBLOTx
 print("DONE!")
 
 
