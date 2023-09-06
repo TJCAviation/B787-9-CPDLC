1890edbee448d0dc3697d628d65c5e14fb02184d
Robin Breitfeld <robin01196@gmail.com>
2023-09-06 17:52:26 +0200

    chore: add files back

---
+++ horizonsim-789/src/model/build/replace.py
@@ -0,0 +1,27 @@
+file_name = "B787_lod1.gltf"
+
+with open(file_name, encoding="utf8") as f:
+    data_lines = f.read()
+
+# 文字列置換
+data_lines = data_lines.replace('"material": 64,', '"material": 3,')#DECALS1
+data_lines = data_lines.replace('"material": 68,', '"material": 4,')#TAIL
+data_lines = data_lines.replace('"material": 58,', '"material": 7,')#LIVERY1
+data_lines = data_lines.replace('"material": 69,', '"material": 6,')#FUSELASGE1/DONE
+data_lines = data_lines.replace('"material": 57,', '"material": 9,')#WINDSHIELD
+data_lines = data_lines.replace('"material": 60,', '"material": 10,')#LIGHTS
+data_lines = data_lines.replace('"material": 61,', '"material": 11,')#FROST
+data_lines = data_lines.replace('"material": 59,', '"material": 13,')#DECALS2
+data_lines = data_lines.replace('"material": 66,', '"material": 21,')#FUSELAGE2/DONE
+data_lines = data_lines.replace('"material": 65,', '"material": 23,')#LIVERY1_3/DONE
+data_lines = data_lines.replace('"material": 67,', '"material": 24,')#HANDLE/DONE
+data_lines = data_lines.replace('"material": 63,', '"material": 22,')#GLASS
+data_lines = data_lines.replace('"material": 61,', '"material": 32,')#HUBLOTx
+print("DONE!")
+
+
+
+
+# 同じファイル名で保存
+with open(file_name, mode="w", encoding="utf8") as f:
+    f.write(data_lines)
