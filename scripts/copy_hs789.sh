49a0b5b39f57d40b136169873f1492436d2eeaa4
Robin Breitfeld <robin01196@gmail.com>
2023-09-11 20:07:29 +0200

    update copy script

--- scripts/copy_hs789.sh
+++ scripts/copy_hs789.sh
@@ -7,8 +7,8 @@ df -h
 #remove directory if it exist
 rm -rvf ./horizonsim-789/build/
 
-mkdir ./horizonsim-789/build
+mkdir -p ./horizonsim-789/build/
 # copy from Horizon B789 model source into build
-cp -rva ./horizonsim-789/src/base/ /horizonsim-789/build/
-cp -rva ./horizonsim-789/src/model/build/ ./horizonsim-789/build/horizonsim-aircraft-787-9/SimObjects/Airplanes/HS_B787_9/model.GE
-cp -rva ./horizonsim-789/src/model/build/ ./horizonsim-789/build/horizonsim-aircraft-787-9/SimObjects/Airplanes/HS_B787_9/model.RR
+cp -rva ./horizonsim-789/src/base/. ./horizonsim-789/build/
+cp -rva ./horizonsim-789/src/model/build/. ./horizonsim-789/build/horizonsim-aircraft-787-9/SimObjects/Airplanes/HS_B787_9/model.GE
+cp -rva ./horizonsim-789/src/model/build/. ./horizonsim-789/build/horizonsim-aircraft-787-9/SimObjects/Airplanes/HS_B787_9/model.RR
