f9487259374ebb0e8353f88b8eaf06979819d80f
Robin Breitfeld <robin01196@gmail.com>
2023-09-11 23:01:54 +0200

    pipeline: use legacy model

--- scripts/copy_hs789.sh
+++ scripts/copy_hs789.sh
@@ -10,5 +10,7 @@ rm -rvf ./horizonsim-789/build/
 mkdir -p ./horizonsim-789/build/
 # copy from Horizon B789 model source into build
 cp -rva ./horizonsim-789/src/base/. ./horizonsim-789/build/
-cp -rva ./horizonsim-789/src/model/build/. ./horizonsim-789/build/horizonsim-aircraft-787-9/SimObjects/Airplanes/HS_B787_9/model.GE
-cp -rva ./horizonsim-789/src/model/build/. ./horizonsim-789/build/horizonsim-aircraft-787-9/SimObjects/Airplanes/HS_B787_9/model.RR
+
+# for now use working legacy model
+cp -rva ./horizonsim-789/src/model/legacy/. ./horizonsim-789/build/horizonsim-aircraft-787-9/SimObjects/Airplanes/HS_B787_9/model.GE
+cp -rva ./horizonsim-789/src/model/legacy/. ./horizonsim-789/build/horizonsim-aircraft-787-9/SimObjects/Airplanes/HS_B787_9/model.RR
