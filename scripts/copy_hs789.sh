9f8ace53b6ef5d6a47220f2d4f0030bc6f353ebc
Robin Breitfeld <robin01196@gmail.com>
2023-09-11 19:52:31 +0200

    update pipeline

--- scripts/copy_hs789.sh
+++ scripts/copy_hs789.sh
@@ -5,17 +5,12 @@ set -ex
 df -h
 
 #remove directory if it exist
-rm -rvf ./build-hs789
+rm -rvf ./horizonsim-789/build/
 
 # create directory
 mkdir -p ./build-hs789/src
 mkdir -p ./build-hs789/out
 
-# copy from Horizon B789 source into build
-cp -rva ./horizonsim-789/src/model/. ./build-hs789/src/model
-
-# create target directory for base aircraft
-mkdir -p ./build-hs789/out/horizonsim-aircraft-787-9
-
-# copy B789 base to out
-cp -rva ./horizonsim-789/src/base/horizonsim-aircraft-787-9/. ./build-hs789/out/horizonsim-aircraft-787-9
+# copy from Horizon B789 model source into build
+cp -rva ./horizonsim-789/src/model/build/ ./horizonsim-789/build/horizonsim-aircraft-787-9/SimObjects/Airplanes/HS_B787_9/model.GE
+cp -rva ./horizonsim-789/src/model/build/ ./horizonsim-789/build/horizonsim-aircraft-787-9/SimObjects/Airplanes/HS_B787_9/model.RR
