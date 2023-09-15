1be6a9f4c626c99c57119d74237ae8ebe155fe7f
Robin Breitfeld <robin01196@gmail.com>
2023-09-15 09:48:42 +0200

    uodate copy task

--- scripts/copy_hs789.sh
+++ scripts/copy_hs789.sh
@@ -10,7 +10,3 @@ rm -rvf ./horizonsim-789/build/
 mkdir -p ./horizonsim-789/build/
 # copy from Horizon B789 model source into build
 cp -rva ./horizonsim-789/src/base/. ./horizonsim-789/build/
-
-# for now use working legacy model
-cp -rva ./horizonsim-789/src/model/legacy/. ./horizonsim-789/build/horizonsim-aircraft-787-9/SimObjects/Airplanes/HS_B787_9/model.GE
-cp -rva ./horizonsim-789/src/model/legacy/. ./horizonsim-789/build/horizonsim-aircraft-787-9/SimObjects/Airplanes/HS_B787_9/model.RR
