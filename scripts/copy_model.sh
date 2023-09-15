e19ca8ef8322ad400f28c4f47769bb2065c8ba0b
TJCAviation <80174397+TJCAviation@users.noreply.github.com>
2023-09-15 21:58:17 +0100

    Revert "Correctly Split engine variants"
    This reverts commit 2c291a20e90b98c08618265e1c7952e7d6d1d2ec.

--- scripts/copy_model.sh
+++ scripts/copy_model.sh
@@ -1,4 +1,5 @@
 set -ex
 
 # copy from Horizon B789 model source into build
-cp -rva ./horizonsim-789/src/model/build/. ./horizonsim-789/build/horizonsim-aircraft-787-9/SimObjects/Airplanes/HS_B787_9-Shared/model
+cp -rva ./horizonsim-789/src/model/build/. ./horizonsim-789/build/horizonsim-aircraft-787-9/SimObjects/Airplanes/HS_B787_9/model.GE
+cp -rva ./horizonsim-789/src/model/build/. ./horizonsim-789/build/horizonsim-aircraft-787-9/SimObjects/Airplanes/HS_B787_9/model.RR
