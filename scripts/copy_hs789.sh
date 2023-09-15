aa15163401e647db12ba1ab9e501b0be661b7d4f
Robin Breitfeld <robin01196@gmail.com>
2023-09-15 12:04:44 +0200

    update build script to ensure build folder is empty

--- scripts/copy_hs789.sh
+++ scripts/copy_hs789.sh
@@ -6,7 +6,9 @@ df -h
 
 #remove directory if it exist
 rm -rvf ./horizonsim-789/build/
+rm -rvf ./horizonsim-789/src/model/build/
 
 mkdir -p ./horizonsim-789/build/
+mkdir -p ./horizonsim-789/src/model/build/
 # copy from Horizon B789 model source into build
 cp -rva ./horizonsim-789/src/base/. ./horizonsim-789/build/
