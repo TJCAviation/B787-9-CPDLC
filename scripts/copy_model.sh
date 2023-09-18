80e05fba380094cbbc0efa8bc29eb6a749403d10
Robin Breitfeld <robin01196@gmail.com>
2023-09-18 16:35:14 +0200

    add kuro files

--- scripts/copy_model.sh
+++ scripts/copy_model.sh
@@ -1,4 +1,4 @@
 set -ex
 
 # copy from Horizon B789 model source into build
-cp -rva ./horizonsim-789/src/model/build/. ./horizonsim-789/build/horizonsim-aircraft-787-9/SimObjects/Airplanes/HS_B787_9_SHARED/model
+cp -rva ./horizonsim-789/src/model/build/. ./horizonsim-789/build/horizonsim-aircraft-787-9/SimObjects/Airplanes/HorizonSim_B787_9_SHARED/model
