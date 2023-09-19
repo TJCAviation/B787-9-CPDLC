fed0c385676590ebc35d02c6e9547c0e0336f916
Kurorin-fs <119593840+Kurorin-fs@users.noreply.github.com>
2023-09-19 22:13:55 +0900

    Update replace.py

--- horizonsim-789/src/model/build/replace.py
+++ horizonsim-789/src/model/build/replace.py
@@ -5,22 +5,22 @@ with open(file_name, encoding="utf8") as f:
 
 # 文字列置換
 data_lines = data_lines.replace('"material": 59,', '"material": 9,')#WINDSHIELD
-data_lines = data_lines.replace('"material": 60,', '"material": 3,')#DECALS1
-data_lines = data_lines.replace('"material": 61,', '"material": 23,')#LIVERY1_3
-data_lines = data_lines.replace('"material": 62,', '"material": 6,')#FUSELASGE1
-data_lines = data_lines.replace('"material": 63,', '"material": 4,')#TAIL
-data_lines = data_lines.replace('"material": 64,', '"material": 21,')#FUSELAGE2
-data_lines = data_lines.replace('"material": 65,', '"material": 24,')#HANDLE
-data_lines = data_lines.replace('"material": 66,', '"material": 13,')#DECALS2
-data_lines = data_lines.replace('"material": 67,', '"material": 7,')#LIVERY1
-#68 M_Belly
-data_lines = data_lines.replace('"material": 69,', '"material": 6,')#FUSELASGE1
-data_lines = data_lines.replace('"material": 70,', '"material": 10,')#LIGHTS
-#71 RIGHT
-data_lines = data_lines.replace('"material": 72,', '"material": 11,')#FROST
-data_lines = data_lines.replace('"material": 73,', '"material": 29,')#HUBLOTx
-#74 Tail_Decals
-data_lines = data_lines.replace('"material": 75,', '"material": 22,')#GLASS
+data_lines = data_lines.replace('"material": 60,', '"material": 7,')#LIVERY1
+data_lines = data_lines.replace('"material": 61,', '"material": 13,')#DECALS2
+data_lines = data_lines.replace('"material": 62,', '"material": 10,')#LIGHTS
+data_lines = data_lines.replace('"material": 63,', '"material": 11,')#FROST
+data_lines = data_lines.replace('"material": 64,', '"material": 29,')#HUBLOTx
+data_lines = data_lines.replace('"material": 65,', '"material": 22,')#GLASS
+data_lines = data_lines.replace('"material": 66,', '"material": 3,')#DECALS1
+data_lines = data_lines.replace('"material": 67,', '"material": 23,')#LIVERY1_3
+data_lines = data_lines.replace('"material": 68,', '"material": 21,')#FUSELAGE2
+data_lines = data_lines.replace('"material": 69,', '"material": 24,')#HANDLE
+data_lines = data_lines.replace('"material": 70,', '"material": 4,')#TAIL
+data_lines = data_lines.replace('"material": 71,', '"material": 6,')#FUSELASGE1
+#72 M_Belly
+#73 Tail_Decals
+data_lines = data_lines.replace('"material": 74,', '"material": 6,')#FUSELASGE1
+#75 RIGHT
 
 
 
