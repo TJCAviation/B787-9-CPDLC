bc21e91ecac78dce4a4d8e7fe33e349fca68d249
Kurorin-fs <119593840+Kurorin-fs@users.noreply.github.com>
2023-09-19 15:10:53 +0900

    Update replace.py

--- horizonsim-789/src/model/build/replace.py
+++ horizonsim-789/src/model/build/replace.py
@@ -13,14 +13,14 @@ data_lines = data_lines.replace('"material": 64,', '"material": 21,')#FUSELAGE2
 data_lines = data_lines.replace('"material": 65,', '"material": 24,')#HANDLE
 data_lines = data_lines.replace('"material": 66,', '"material": 13,')#DECALS2
 data_lines = data_lines.replace('"material": 67,', '"material": 7,')#LIVERY1
-
-data_lines = data_lines.replace('"material": 70,', '"material": 6,')#FUSELASGE1
-data_lines = data_lines.replace('"material": 71,', '"material": 10,')#LIGHTS
-
+#68 M_Belly
+data_lines = data_lines.replace('"material": 69,', '"material": 6,')#FUSELASGE1
+data_lines = data_lines.replace('"material": 70,', '"material": 10,')#LIGHTS
+#71 RIGHT
 data_lines = data_lines.replace('"material": 72,', '"material": 11,')#FROST
 data_lines = data_lines.replace('"material": 73,', '"material": 29,')#HUBLOTx
-
-data_lines = data_lines.replace('"material": 76,', '"material": 22,')#GLASS
+#74 Tail_Decals
+data_lines = data_lines.replace('"material": 75,', '"material": 22,')#GLASS
 
 
 
