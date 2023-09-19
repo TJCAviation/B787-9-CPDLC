b4ce402faba691539ea81ddbe46a106c7e6bc6f6
Robin Breitfeld <robin01196@gmail.com>
2023-09-19 08:13:30 +0200

    chore(scripts): remove tailing dots

--- scripts/copy_model.sh
+++ scripts/copy_model.sh
@@ -1,5 +1,5 @@
 set -ex
 
 # copy from Horizon B789 model source into build
-cp -rva ./horizonsim-789/src/model/build/*.bin. ./horizonsim-789/build/horizonsim-aircraft-787-9/SimObjects/Airplanes/HorizonSim_B787_9_SHARED/model
-cp -rva ./horizonsim-789/src/model/build/*.gltf. ./horizonsim-789/build/horizonsim-aircraft-787-9/SimObjects/Airplanes/HorizonSim_B787_9_SHARED/model
+cp -rva ./horizonsim-789/src/model/build/*.bin ./horizonsim-789/build/horizonsim-aircraft-787-9/SimObjects/Airplanes/HorizonSim_B787_9_SHARED/model
+cp -rva ./horizonsim-789/src/model/build/*.gltf ./horizonsim-789/build/horizonsim-aircraft-787-9/SimObjects/Airplanes/HorizonSim_B787_9_SHARED/model
